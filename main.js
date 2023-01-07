8c946c7c7d66223021bb069d119aa0132d19d3d2
SuperCoolGamer <103328500+SuperCoolGamer@users.noreply.github.com>
2023-01-06 19:59:12 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/xCwJFMrj/images-removebg-preview.png')
 }
 
 function setup() {
@@ -20,6 +23,10 @@ function modelLoaded(){
 
 function draw(){
 image(video, 0, 0, 300, 300);
+fill(255,0,0);
+stroke(255,0,0);
+circle(noseX, noseY, 20);
+image(clown_nose, noseX-15, noseY-15, 30,30);
 }
 
 function take_snapshot(){
@@ -29,6 +36,8 @@ function take_snapshot(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x)
         console.log("nose y = " + results[0].pose.nose.y)
     }
